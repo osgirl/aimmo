221fe38a1b562441e5079c20c92a399bad9f8b5b
Niket Shah <masterniket@gmail.com>
2018-11-26 10:45:15 +0000

    Remove coverage code in setup.py and move test requires into Pipfile

--- aimmo-game/setup.py
+++ aimmo-game/setup.py
@@ -1,17 +1,7 @@
 # -*- coding: utf-8 -*-
 from setuptools import find_packages, setup
-import coverage
 import sys
 import os
-
-
-withcoverage = os.environ.get('WITH_COVERAGE')
-
-if withcoverage == 'True':
-    print("starting code coverage engine")
-    coveragedatafile = ".coverage"
-    cov = coverage.Coverage(config_file=False)
-    cov.start()
 
 
 setup(
@@ -30,19 +20,6 @@ setup(
     setup_requires=[
         "pytest-runner"
     ],
-    tests_require=[
-        'pytest',
-        'pytest-asyncio',
-        'asynctest',
-        'httmock',
-        'mock',
-        'hypothesis'
-    ],
     test_suite='tests',
     zip_safe=False,
 )
-
-if withcoverage == 'True':
-    print("saving coverage stats")
-    cov.save()
-    print("exiting program")
